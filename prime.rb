12d0c287a8be01fa1210997a4282e5e224d2309f
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-20 05:54:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,39 +1,9 @@
-def prime?(num)
-  num <= 1 
-     true 
-     if
-     else 
-       false 
-end 
-# def prime? (n)
-#     if n <= 1
-#         false
-#     elsif n == 2
-#         true
-#     else 
-#         (2..n/2).none? { |i| n % i == 0}
-#     end
-# end
-# the answer but i dont like the code --- dont understand this     1 
-
-
-
-# def prime?(num)
-#   if num <= 1 
-#     false 
-#   else num == 2 
-#     true 
-# end 
-# end 
-# this one returns true for prime numbers BUT DOES NOT returns false for non-prime 
-
-# def prime?(num)
-#   if num <= 1 
-#     false 
-#   elsif num == 2 
-#     true 
-#   else 
-#     false 
-# end 
-# end 
-# this one returns false for non-prime numbers BUT DOES NOT return true for prime number 
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
+end
